adacca5438b20cda4c0b404e5861e41350c73999
samuelnata <franca_borges@hotmail.com>
2017-08-12 15:46:31 -0300

    Change use of 'Apache Poi Sheet' to new SheetWindow, improvind memory usage, and rename Buffer to better code understanding

--- src/main/java/br/telesmeter/service/DataProducer.java
+++ src/main/java/br/telesmeter/service/DataProducer.java
@@ -2,44 +2,39 @@ package br.telesmeter.service;
 
 import java.util.ArrayList;
 
-import org.apache.poi.ss.usermodel.Sheet;
-
 import br.telesmeter.domain.AbstractData;
 import br.telesmeter.sheetdatareader.DataCapture;
+import br.telesmeter.sheetdatareader.SheetWindow;
 import br.telesmeter.utils.Buffer;
 
 public class DataProducer extends Thread{
-	private Buffer<AbstractData> fixedBuffer;
+	private Buffer<AbstractData> objectsToPersistBuffer;
 	private DataCapture dataCapture;
 	private String sourceFile; 
 	
 	public DataProducer(Buffer<AbstractData> fb, DataCapture dc, String fn){
-		this.fixedBuffer = fb;
+		this.objectsToPersistBuffer = fb;
 		this.dataCapture = dc; 
 		this.sourceFile = fn;
 	}
 	
 	public void run(){
-		Sheet sheet = dataCapture.getSheetFromSourceFile(sourceFile, -1);
-		int limit = dataCapture.getNumberOfNoEmptyRows(sheet);
+		SheetWindow sheet = dataCapture.getSheetFromSourceFile(sourceFile, -1, 100);
+		sheet.start();
 		int step = 100;
-		ArrayList<AbstractData> list;
+		ArrayList<AbstractData> objectsToPersist = dataCapture.readDataFromSheet(sheet, step);
 		
-		for( int begin=0 ; begin<limit ; begin+=step ){
-			
-			step = (begin+step)<limit ? step : (limit-begin);
-			list = dataCapture.readDataFromSheet(sheet, begin, step);
-			
+		while( !objectsToPersist.isEmpty() ){
 			try {
-				while( !list.isEmpty() ){
-					fixedBuffer.produce(list.remove(0));
+				while( !objectsToPersist.isEmpty() ){
+					objectsToPersistBuffer.produce(objectsToPersist.remove(0));
 				}
 			} catch (InterruptedException e) {
 					e.printStackTrace();
 			}
+			objectsToPersist = dataCapture.readDataFromSheet(sheet, step);
 		}
-		
-		fixedBuffer.close();
+		objectsToPersistBuffer.close();
 	}
 	
 }
